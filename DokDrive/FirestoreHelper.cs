b4e448c21fe322910b6bbcd6eb8adb938a0f9029
Wonka <willie.lima_lvs@hotmail.com>
2023-11-02 00:11:20 -0300

    Commit para upar o projeto no github

---
+++ DokDrive/FirestoreHelper.cs
@@ -0,0 +1,40 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DokDrive
+{
+    internal class FirestoreHelper
+    {
+        static string fireconfig = @"
+        {
+          ""type"": ""service_account"",
+          ""project_id"": ""dokdrive-7a197"",
+          ""private_key_id"": ""d84725ed6bc10acddcd5c855bfb0b44f64533619"",
+          ""private_key"": ""-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDS5b35P8WZ+PvN\ns9e+MHO+InPjOtWEnvk+sNOG+9cADcwmHM+82KiaxxpCXCPY+Z9pUbKrN8TpQmdQ\nup0xUYf3XFuG7li391/XefEupf2miGt2NxPE/yH+iiGrM4skCHkQPAdEKUasRXi5\nmxP9FbF4zbCtXi96kd2NYQTdVoPzLDSCpXDJdOH4zfj/odcmHpVjtHhrn8Scptkm\nawjUgvmnof//1SKo/z/2z3ke2GUgbHK3pyuv9nuKNxoH81TVtRuTSQ7E5sOaiY6j\nuSbZbO7BWUHXtZnNyVZwZB1Wi9AvcZxRPc+pH/EecT9a3LHuPthb5NwpzmZaE5hQ\nLu95ZKeNAgMBAAECggEAIPBEbkYlSuHOFCfbUYdYQHE5l9sTx5mbtIyiEPJK92Vw\nReCYmnZdSQVBRu2EpMoB+Y1Kwj6c2NCn+b4XuIDzIzFv0yBtR1hRxgHz5xUCS40B\nea5q/kjZyptQ3GAZwng3Tk+N0/m9YcEc9KIxqkTF2egISN+Nu09GYuMtAxPU2nj7\npMxXwAr0FExU9YlNQcGzSxVZWCOpY/tJKLM/Gy6R2CUVJKMxi0yBLZH6QqWNCHFa\nqbJrCNstccrVXZ7draGfeIq/9Jzb3Qp0u5IMlvJCFUisr5vC9AeJe2vhz0xVzdVD\nNwHaHPj+XDCOB7qF7eDyY6uykxA7T3Ic748oXmaceQKBgQDuTCdPzDIXKgm7/+PP\ntkh6bOjObZrLc9ndAm4cMMseG8E6uy7jrd31vIROYZpMFtKPniqXT970LTYWuuf+\nW6A8Ofk4hxdq6JU6/x9k7dY27v/+N0v5XIXDCKHgt+oQulF6wE1/XJ2fJsShypWB\nXuOVaDzAJwJEd1PkSsbR0X7hpQKBgQDikIFhBmWo7ltdAmMKSvdC96E98p1SwWdF\nO9gQXMWYCbtpDj5Pk1eM8OcsZjlCktrK62P2rZTN8xjIA9bglyxvmS7PjGDh6Szz\nG8Y3OBSenDN2M/mkJjIP3tlb/zZIL2BLRtgDWKe6NmCfSD8x5obqrLmd5u2cpqg1\ne6jhzFz5yQKBgHt2g0upzS4JPr/j5YFBvS2I7dm+GdxdarxCPRBoC2TPFn86aiHW\nfFCMWrRNh2by0CdO2FxLKoWyQvrl3bsAd/WcR86M14oxdNf7RKXDQCB4FbP9rGRH\nFpqlB1Hpl+EQ15RUvBJHxvalXG1DC9Dae+l19zvLOjaE6Lc4MuiUtTttAoGATn/J\nBAgGYyCq8S/phuBwiXhpGqCcF9dA48EXXJv4LnR4xa6UsleXxxKU1lJY1pMxUYt7\naDW+UBlBZdPGuKxITiZiDLFTxBJZa6Yc6Hb61fXC8tqRTWmagNsyrmhfs2UO1SYn\npAksnB1JHwcWNkDsUBUmfupLO7KWp2lAG3QGZtkCgYAu9lYZWHEBdhM/GzAFSBq4\nQ8VT5wwKlRsmO5ZWVJ+rpCpJLIKbIxRyTXOTGI35BeYLbmZOmIFMjtsc3cMczOsB\n7KU/vuO5MCxxg3Fmbg5VkdmjjYJiCff7fKlXCYCNPQ4bAQ0uOeXcAgjRg9qYabF+\nqxPJfI3bOzam9dl9CutKYA==\n-----END PRIVATE KEY-----\n"",
+          ""client_email"": ""firebase-adminsdk-qyoe2@dokdrive-7a197.iam.gserviceaccount.com"",
+          ""client_id"": ""118052744113219719126"",
+          ""auth_uri"": ""https://accounts.google.com/o/oauth2/auth"",
+          ""token_uri"": ""https://oauth2.googleapis.com/token"",
+          ""auth_provider_x509_cert_url"": ""https://www.googleapis.com/oauth2/v1/certs"",
+          ""client_x509_cert_url"": ""https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-qyoe2%40dokdrive-7a197.iam.gserviceaccount.com"",
+          ""universe_domain"": ""googleapis.com""
+        }
+        ";
+        static string filepath = "";
+        public static FirestoreDb? Database { get; private set; }
+
+        public static void SetEnvironmentVariable()
+        {
+            filepath = Path.Combine(Path.GetTempPath(), Path.GetFileNameWithoutExtension(Path.GetRandomFileName())) + ".json";
+            File.WriteAllText(filepath, fireconfig);
+            File.SetAttributes(filepath, FileAttributes.Hidden);
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", filepath);
+            Database = FirestoreDb.Create("dokdrive-7a197");
+            File.Delete(filepath);
+        }
+    }
+}
---
+++ DokDrive/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DokDrive
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textEmail = new TextBox();
+            textSenha = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            buttonLogin = new Button();
+            buttonSignUpPage = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 50.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(272, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(252, 80);
+            label1.TabIndex = 0;
+            label1.Text = "Demo";
+            label1.Click += label1_Click;
+            // 
+            // textEmail
+            // 
+            textEmail.Location = new Point(243, 138);
+            textEmail.Name = "textEmail";
+            textEmail.PlaceholderText = "  Insira seu email";
+            textEmail.Size = new Size(307, 23);
+            textEmail.TabIndex = 1;
+            // 
+            // textSenha
+            // 
+            textSenha.Location = new Point(243, 214);
+            textSenha.Name = "textSenha";
+            textSenha.PlaceholderText = "  ********";
+            textSenha.Size = new Size(307, 23);
+            textSenha.TabIndex = 2;
+            textSenha.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(187, 141);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 17);
+            label2.TabIndex = 3;
+            label2.Text = "Email: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(187, 217);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 17);
+            label3.TabIndex = 4;
+            label3.Text = "Senha: ";
+            // 
+            // buttonLogin
+            // 
+            buttonLogin.Location = new Point(327, 274);
+            buttonLogin.Name = "buttonLogin";
+            buttonLogin.Size = new Size(122, 30);
+            buttonLogin.TabIndex = 5;
+            buttonLogin.Text = "Login";
+            buttonLogin.UseVisualStyleBackColor = true;
+            buttonLogin.Click += buttonLogin_Click;
+            // 
+            // buttonSignUpPage
+            // 
+            buttonSignUpPage.Location = new Point(327, 331);
+            buttonSignUpPage.Name = "buttonSignUpPage";
+            buttonSignUpPage.Size = new Size(122, 30);
+            buttonSignUpPage.TabIndex = 6;
+            buttonSignUpPage.Text = "Inscreva-se";
+            buttonSignUpPage.UseVisualStyleBackColor = true;
+            buttonSignUpPage.Click += buttonSignUpPage_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonSignUpPage);
+            Controls.Add(buttonLogin);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textSenha);
+            Controls.Add(textEmail);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textEmail;
+        private TextBox textSenha;
+        private Label label2;
+        private Label label3;
+        private Button buttonLogin;
+        private Button buttonSignUpPage;
+    }
+}
